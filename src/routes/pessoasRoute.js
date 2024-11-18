a39058ec982a79725922612cfd1ddb17273e4b85
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-11-18 17:43:45 -0300

    Finalizando o CRUD

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -6,5 +6,10 @@ const pessoaController = new PessoaController();
 const router = Router();
 
 router.get('/pessoas', (req, res) => pessoaController.pegaTodos(req, res));
+//router.get('/pessoas/:id', (req, res) => pessoaController.pegarUmPorId(req, res));
+//router.post('/pessoas', (req, res) => pessoaController.criarNovo(req, res));
+router.put('/pessoas/:id', (req, res) => pessoaController.atualiza(req, res));
+//router.delete('/pessoas/:id', (req, res) => pessoaController.excluir(req, res));
+
 
 module.exports = router;
