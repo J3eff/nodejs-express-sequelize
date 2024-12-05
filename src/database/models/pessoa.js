f36d01e4994721d51e8f013696eb23177a6e2fac
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-12-05 18:47:06 -0300

    Validação customizada

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -1,4 +1,6 @@
 'use strict';
+const isCpfValido = require('../../utils/validaCpfHelper.js');
+
 const {
   Model
 } = require('sequelize');
@@ -17,7 +19,15 @@ module.exports = (sequelize, DataTypes) => {
   }
   
   Pessoa.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [3, 30],
+          msg: 'O campo nome deve ter no mínimo 3 caracteres'
+        }
+      }
+    },
     email: {
       type: DataTypes.STRING,
       validate: {
@@ -27,7 +37,14 @@ module.exports = (sequelize, DataTypes) => {
         }
       }
     },
-    cpf: DataTypes.STRING,
+    cpf: {
+      type: DataTypes.STRING,
+      validate: {
+        cpfEhValido: (cpf) => {
+          if(!isCpfValido(cpf)) throw new Error('Numero de CPF inválido.');
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     role: DataTypes.STRING
   }, {
