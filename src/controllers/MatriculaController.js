d35d36c320ea8defb02edbe810708e10586af0fb
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-12-17 00:14:17 -0300

    Agrupando com literais

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -1,3 +1,4 @@
+const Sequelize = require('sequelize');
 const Controller = require('./Controller.js');
 const MatriculaServices = require('../services/MatriculaServices.js');
 
@@ -6,14 +7,18 @@ const matriculaServices = new MatriculaServices();
 class MatriculaController extends Controller {
   constructor() {
     super(matriculaServices);
-  }  
-  
+  }
+
   async pegaMatriculasPorEstudante(req, res) {
-    const { estudante_id } = req.params;    
+    const { estudante_id } = req.params;
     try {
       const listaMatriculaPorEstudante = await matriculaServices.pegaEContaRegistros({
-        estudante_id: Number(estudante_id),
-        status: 'matriculado'
+        where: {
+          estudante_id: Number(estudante_id),
+          status: 'matriculado'
+        },
+        limit: 2,
+        order: [['id', 'DESC']]
       });
       return res.status(200).json(listaMatriculaPorEstudante);
     } catch (erro) {
@@ -25,9 +30,12 @@ class MatriculaController extends Controller {
     const lotacaoCurso = 2;
     try {
       const cursosLotados = await matriculaServices.pegaEContaRegistros({
-        status: 'matriculado',        
+        where: { status: 'matriculado' },
+        attributes: ['curso_id'],
+        group: ['curso_id'],
+        having: Sequelize.literal(`count(curso_id) >= ${lotacaoCurso}`)
       });
-      return res.status(200).json(cursosLotados);
+      return res.status(200).json(cursosLotados.count);
     } catch (erro) {
       return res.status(500).json({ erro: erro.message });
     }
