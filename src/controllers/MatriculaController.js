d0d75148a35fbf6d4c5ebf91aef919fb00ac4566
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-12-16 23:42:29 -0300

    Funções agregadoras

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -6,7 +6,20 @@ const matriculaServices = new MatriculaServices();
 class MatriculaController extends Controller {
   constructor() {
     super(matriculaServices);
-  }    
+  }  
+  
+  async pegaMatriculasPorEstudante(req, res) {
+    const { estudante_id } = req.params;    
+    try {
+      const listaMatriculaPorEstudante = await matriculaServices.pegaEContaRegistros({
+        estudante_id: Number(estudante_id),
+        status: 'matriculado'
+      });
+      return res.status(200).json(listaMatriculaPorEstudante);
+    } catch (erro) {
+      return res.status(500).json({ erro: erro.message });
+    }
+  }
 }
 
 module.exports = MatriculaController;
--- src/services/Services.js
+++ src/services/Services.js
@@ -21,6 +21,10 @@ class Services {
     return dataSource[this.model].findOne({ where: { ...where } });
   }
 
+  async pegaEContaRegistros(where) {
+    return dataSource[this.model].findAndCountAll({ where: { ...where } /*, order: [['id', 'DESC']] */});
+  }
+
   async criaRegistro(dadosDoRegistro) {
     return dataSource[this.model].create(dadosDoRegistro);
   }
